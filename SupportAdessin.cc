c20c33f8b24883b0930cb8c57034bb0c28f1477b
Mattcas23 <162562141+Mattcas23@users.noreply.github.com>
2024-04-11 12:15:04 +0200

    semaine6

---
+++ SupportAdessin.cc
@@ -0,0 +1,4 @@
+#include <iostream>
+#include <vector>
+#include "SupportADessin.h"
+using namespace std;
---
+++ dessinable.cc
@@ -0,0 +1,5 @@
+#include <iostream>
+#include <vector>
+#include "dessinable.h"
+#include "SupportAdessin.h"
+using namespace std;
